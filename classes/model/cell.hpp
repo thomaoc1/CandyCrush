5e8d78a8da363edfe36f152aeac8e36fcab0fec8
thomas <thomas.ocuilleanain@ulb.be>
2022-06-27 17:34:18 +0300

    Methods added to reduce stringing method calls

--- classes/model/cell.hpp
+++ classes/model/cell.hpp
@@ -8,9 +8,11 @@
 class Cell {
     std::shared_ptr<GameComponent> occupied;
 public:
-    Cell(const std::shared_ptr<GameComponent> &gc) : occupied{gc} {}
     void setOccupied(const std::shared_ptr<GameComponent> &gc) {occupied = gc;}
     std::shared_ptr<GameComponent> getOccupied() const {return occupied;}
+    void setNeighbours(const std::vector< std::shared_ptr<GameComponent> > &nb) {occupied->setNeighbours(nb);}
+    std::vector< std::shared_ptr<GameComponent> > getNeighbours() const {return occupied->getNeighbours();}
+    std::string package() const {return occupied->package();}
 };
 
 

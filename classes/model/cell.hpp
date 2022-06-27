6086a5f9b18a127f56624a8ed85f1f79c4d32fe3
thomas <thomas.ocuilleanain@ulb.be>
2022-06-27 10:18:18 +0200

    Neighbours attribute and setter

--- classes/model/cell.hpp
+++ classes/model/cell.hpp
@@ -4,11 +4,16 @@
 #include "../constants/constants.hpp"
 
 #include <string>
+#include <vector>
+#include <memory>
 
 class Cell {
+    std::vector< std::shared_ptr<Cell> > neighbours;
 public:
     virtual ~Cell() {};
     virtual std::string package() const=0;
+    virtual void setNeighbours(const std::vector< std::shared_ptr<Cell> > &nbs) {neighbours = nbs;}
+    virtual std::vector< std::shared_ptr<Cell> > getNeighbours() const {return neighbours;}
 };
 
 #endif

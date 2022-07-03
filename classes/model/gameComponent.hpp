606042a15a4966f45d80a8b54d9052667108135f
tocuille <thomas.ocuilleanain@ulb.be>
2022-07-03 18:24:24 +0200

    Part of nb logic change

--- classes/model/gameComponent.hpp
+++ classes/model/gameComponent.hpp
@@ -8,12 +8,9 @@
 #include <memory>
 
 class GameComponent {
-    std::vector< std::shared_ptr<GameComponent> > neighbours;
 public:
     virtual ~GameComponent() {};
     virtual std::string package() const=0;
-    virtual void setNeighbours(const std::vector< std::shared_ptr<GameComponent> > &nbs) {neighbours = nbs;}
-    virtual std::vector< std::shared_ptr<GameComponent> > getNeighbours() const {return neighbours;}
 };
 
 #endif

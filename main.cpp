68511ce3a18101421fdbe042481f4c36c27bb1d9
tocuille <thomas.ocuilleanain@ulb.be>
2022-07-06 00:19:58 +0200

    Test

--- main.cpp
+++ main.cpp
@@ -7,8 +7,10 @@ int main() {
     //std::cout << grid.checkSwap({0,2},{1,2}) << std::endl;
     //grid.display();
     //
-    grid.clean();
+    //grid.clean();
+    //grid.display();
+    std::cout << grid.checkSwap({6,1},{5,1}) << std::endl;
     grid.display();
-
+    grid.clean();
     return 0;
 }

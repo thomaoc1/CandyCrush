b11a7d12eba452d2caa7b4ddeb0b2360bfab0619
tocuille <thomas.ocuilleanain@ulb.be>
2022-07-19 19:34:00 +0200

    Observer

---
+++ classes/control/gridObserver.hpp
@@ -0,0 +1,29 @@
+#ifndef GRID_OBSERVER_HPP
+#define GRID_OBSERVER_HPP
+
+class GridDisplay;
+class Grid;
+
+#include "../view/gridDisplay.hpp"
+
+#include <vector>
+#include <iostream>
+
+class GridObserver {
+    GridDisplay * gridDisplay;
+public:
+    void setObserver(GridDisplay * gridDisplay) {this->gridDisplay = gridDisplay;}
+
+    void notifySwap(const Point &/*coord1*/, const Point &/*coord2*/) const {}
+
+    void notifyDropDown(const std::vector<Point> &/*drops*/) const {}
+    void notifyDropLeft(const Point &/*coord*/) const {}
+    void notifyDropRight(const Point &/*coord*/) const {}
+    
+    using CoordColour = std::pair<Point, int>;
+    void notifyFill(const std::vector<CoordColour> &/*fills*/) const {}
+
+    void notifyPop(const std::vector<Point> &/*pops*/) const {}
+};
+
+#endif

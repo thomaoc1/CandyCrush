4d3014ef74bef4f9b5930ab520a70f00a00efc7d
tocuille <thomas.ocuilleanain@ulb.be>
2022-07-25 21:03:02 +0200

    Faster animations

--- classes/view/animation/animation.hpp
+++ classes/view/animation/animation.hpp
@@ -26,7 +26,7 @@ class ComponentDisplay;
 class Animation {
     ComponentDisplay * component;
 protected:
-    const double animationTime = 30;
+    const double animationTime = 20;
     double time = 0;
 public:
     Animation(ComponentDisplay * component) : component{component} {}

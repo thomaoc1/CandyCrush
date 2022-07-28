4f421df3b083ee23a77106b78b6dca4f4596880f
tocuille <thomas.ocuilleanain@ulb.be>
2022-07-28 22:40:42 +0200

    noice

--- main.cpp
+++ main.cpp
@@ -1,11 +1,8 @@
-#include "classes/view/mainWindow.hpp"
+#include "classes/view/windows/mainWindow.hpp"
 
-#include <FL/Fl.H>
-#include <FL/Fl_Window.H>
-#include <FL/Fl_Box.H>
 
 int main(int argc, char *argv[]) {
-  //srand(time(0));
+  srand(time(0));
   MainWindow window;
   window.show(argc, argv);
   return Fl::run();

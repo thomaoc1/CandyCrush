94c321326d1b7aa1460c33a33fcf632acd9dfa09
mchretie <marcus.chretien@ulb.be>
2022-07-29 15:35:01 +0200

    frostingDisplay

---
+++ classes/view/components/frostingDisplay.hpp
@@ -0,0 +1,25 @@
+#ifndef FROSTING_HPP
+#define FROSTING_HPP
+
+#include "componentDisplay.hpp"
+#include "../shapes/rectangle.hpp"
+#include "../text/textDisplay.hpp"
+
+
+
+class FrostingDisplay : public ComponentDisplay {
+    int layers = 2;
+    TextDisplay text;
+
+public:
+    FrostingDisplay(const Point &center, int layer) noexcept 
+        : ComponentDisplay(std::make_shared<Rectangle>(center, Constants::FROSTING1, Constants::INTER_CELL, FL_FLAT_BOX)),
+                            layers{layer},
+                            text{getCenter(), std::to_string(layers), FL_HELVETICA, static_cast<int>(getSize() / 1.5)} {}
+
+    // DEBUG
+    std::string type() const override {return layers==1 ? "F1" : "F2" ;}
+
+};
+
+#endif

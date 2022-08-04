29061d10e5ed8fe6ec50363d35c221dfd3975678
mchretie <marcus.chretien@ulb.be>
2022-08-04 21:49:46 +0200

    cleared the couts

--- classes/view/text/scoreDisplay.cpp
+++ classes/view/text/scoreDisplay.cpp
@@ -4,14 +4,9 @@
 void ScoreDisplay::draw() const {
     fl_color(FL_BLACK);
     fl_font(FL_HELVETICA, size);
-    std::cout<<"size : "<< size << std::endl;
     int width = 0 , height = 0;
     // Sometimes 50,36 sometimes 102,18
     fl_measure(text.c_str(), width, height, false);
-
-
-    std::cout<<"w,h :"<<width<<" "<<height<<std::endl;
-    std::cout<<"center "<<center.x<<" "<<center.y<<std::endl;
 
     fl_draw(text.c_str(), center.x - width / 2, center.y + ( 2 * height ));
 }   

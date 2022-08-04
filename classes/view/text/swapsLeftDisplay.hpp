1cc0d18625afbb38a2e50a4f3ca56ddd80c05cc9
mchretie <marcus.chretien@ulb.be>
2022-08-04 23:07:54 +0200

    swapsleftDisplay

---
+++ classes/view/text/swapsLeftDisplay.hpp
@@ -0,0 +1,22 @@
+#ifndef SWAPS_LEFT_DISPLAY_HPP 
+#define SWAPS_LEFT_DISPLAY_HPP
+
+#include "text.hpp"
+
+#include "../../constants/constants.hpp"
+
+class SwapsLeftDisplay : public Text {
+
+public:
+    SwapsLeftDisplay(const Point &center, int size, int swaps=0)
+        : Text{center, "Sawps left: " + std::to_string(swaps), FL_HELVETICA, size} {}
+    SwapsLeftDisplay() = default;
+    SwapsLeftDisplay(const SwapsLeftDisplay &) = default;
+    SwapsLeftDisplay(SwapsLeftDisplay &&) = default;
+
+    void setText(const std::string &message) override {text = "Swaps left: " + message;}
+    void setText(std::string &&message) override {text = "Swaps left: " + message;}
+    void draw() const override;
+};
+
+#endif
